e78ef0c9939b87e7d24f2213e5cd0f4ca30a551e
Duane Creates <duanecreates@gmail.com>
2020-10-23 23:37:14 +0200

    Add settings component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-
+  <div class="app-container">
+    <Settings v-model="size"/>
   </div>
 </template>
 
 <script>
 
+import Settings from '@/components/Settings.vue';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Settings,
+  },
+  data() {
+    return {
+      size: 5,
+    };
+  },
+  methods: {},
 };
 </script>
 
 <style lang="scss">
+.app-container {
+
+}
 </style>
---
+++ src/components/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="settings-container">
+    <input type="range" min="3" max="50" :value="modelValue"
+           @input="(event) => $emit('update:modelValue', +event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  props: {
+    modelValue: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.settings-container {
+
+}
+</style>
